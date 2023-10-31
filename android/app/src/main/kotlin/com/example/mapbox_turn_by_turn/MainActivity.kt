90fa340c6215dba1f5a7bf303655d4b040f63ad2
Aarav Jain <aaravjain272003@gmail.com>
2023-10-31 13:28:47 -0700

    fresh start once again

--- android/app/src/main/kotlin/com/example/mapbox_turn_by_turn/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mapbox_turn_by_turn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,27 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'ui/splash.dart';
-
 late SharedPreferences sharedPreferences;
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   sharedPreferences = await SharedPreferences.getInstance();
   await dotenv.load(fileName: "assets/config/.env");
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Mapbox Flutter',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primarySwatch: Colors.indigo,
-            textTheme:
-                GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)),
-        home: const Splash());
+    return const MaterialApp(
+      title: 'Audible Maps',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
   }
 }
